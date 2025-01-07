0443d7deef36904ac02feb8a99b853a524d512e8
Endrit <endrit.makolli7@gmail.com>
2025-01-07 15:23:01 +0100

    Error fix

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,30 +1,70 @@
-import React, { useEffect, useState } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from 'react';
 import axios from 'axios';
-import { Button, Header, List } from 'semantic-ui-react';
+import { Container } from 'semantic-ui-react';
+import { Product } from '../models/product';
+import NavBar from './NavBar';
+import ProductDashboard from '../../features/products/dashboard/ProductDashboard';
+import { v4 as uuid } from 'uuid';
 
 function App() {
-  const [activities, setActivities] = useState([]);
+  const [products, setProducts] = useState<Product[]>([]);
+  const [selectedProduct, setSelectedProduct] = useState<Product | undefined>(undefined);
+  const [editMode, setEditMode] = useState(false);
 
   useEffect(() => {
-    axios.get('http://localhost:5000/api/activities')
+    axios.get<Product[]>('http://localhost:5000/api/products')
       .then(response => {
-        setActivities(response.data);
+        setProducts(response.data);
       })
   }, [])
 
+  function handleSelectProduct(id: string) {
+    setSelectedProduct(products.find(x => x.id === id));
+  }
+
+  function handleCancelSelect() {
+    setSelectedProduct(undefined);
+  }
+
+  function handleFormOpen(id?: string) {
+    id ? handleSelectProduct(id) : handleCancelSelect();
+    setEditMode(true);
+  }
+
+  function handleFormClose() {
+    setEditMode(false);
+  }
+
+  function handleDeleteProduct(id: string) {
+    setProducts([...products.filter(x => x.id !== id)])
+  }
+
+  function handleCreateOrEditProduct(product: Product) {
+    product.id
+      ? setProducts([...products.filter(x => x.id !== product.id), product])
+      : setProducts([...products, { ...product, id: uuid() }]);
+    setEditMode(false);
+    setSelectedProduct(product);
+  }
+
   return (
-    <div>
-        <Header as='h2' icon='users' content='Reactivities' />
-        <List>
-          {activities.map((activity: any) => (
-            <List.Item key={activity.id}>
-              {activity.title}
-            </List.Item>
-          ))}
-        </List>
-    </div>
+    <>
+    <h1>Hellooooooo</h1>
+      <NavBar openForm={handleFormOpen} />
+      <Container style={{ marginTop: '7em' }}>
+        <ProductDashboard
+          products={products}
+          selectedProduct={selectedProduct}
+          selectProduct={handleSelectProduct}
+          cancelSelectProduct={handleCancelSelect}
+          editMode={editMode}
+          openForm={handleFormOpen}
+          closeForm={handleFormClose}
+          createOrEdit={handleCreateOrEditProduct}
+          deleteProduct={handleDeleteProduct}
+        />
+      </Container>
+    </>
   );
 }
 
--- client-app/src/main.tsx
+++ client-app/src/main.tsx
@@ -4,6 +4,7 @@ import './app/layout/styles.css'
 import 'semantic-ui-css/semantic.min.css'
 import App from './app/layout/App'
 
+
 createRoot(document.getElementById('root')!).render( // class the root and renders it
   <StrictMode>
     <App />
